626798078f8280295865c000fb23b0ee3dc81a7b
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2011-04-26 13:47:34 +0200

    Adds customized aliases for relative_path_to helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,26 @@
 include Nanoc3::Helpers::Capturing
 include Nanoc3::Helpers::LinkTo
+
+def rpt(path)
+  relative_path_to(path)
+end
+
+def layout_img(filename)
+  relative_path_to("/images/layout/#{filename}")
+end
+
+def icons_img(filename)
+  relative_path_to("/images/icons/#{filename}")
+end
+
+def content_img(filename)
+  relative_path_to("/images/content/#{filename}")
+end
+
+def css(filename)
+  relative_path_to("/css/#{filename}")
+end
+
+def js(filename)
+  relative_path_to("/js/#{filename}")
+end
